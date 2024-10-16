f656aa84c2a4240e1016d027dc1ead4319f0bd4f
Jarek <jaroslaw.majka@pm.me>
2024-10-16 11:46:36 +0200

    Helper function added for creating linked list

--- linked_lists/linked_list_node.py
+++ linked_lists/linked_list_node.py
@@ -14,3 +14,25 @@ class Node:
     def __init__(self, data):
         self.data = data
         self.next = None
+
+
+def create_five_element_list():
+    '''
+        Helper function used in the exercises for faster creating of five
+        elements Linked List
+    '''
+    linked_list = LinkedList()
+    first_node = Node(1)
+    second_node = Node(2)
+    third_node = Node(3)
+    fourth_node = Node(4)
+    fifth_node = Node(5)
+
+    # `first_node` will be assigned to the head, of the linked list
+    linked_list.head = first_node
+
+    # Other nodes will be assigned to the next, of each previous node
+    first_node.next = second_node
+    second_node.next = third_node
+    third_node.next = fourth_node
+    fourth_node.next = fifth_node
