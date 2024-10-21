6de40a944c741da9b514ff1c7a5d9e2bdd267037
Jarek <jaroslaw.majka@pm.me>
2024-10-21 21:36:40 +0200

    Code reformatted for new standards

--- algorithms/trees/insert_bst.py
+++ algorithms/trees/insert_bst.py
@@ -16,16 +16,17 @@ def insert_into_bst(root, value):
     return root
 
 
-# Example usage
-# Create brand new BST
-print('Create new BST with passed values')
-bst = None
-tree_values = [5, 3, 8, 2, 4, 7, 9]
-for curr_value in tree_values:
-    bst = insert_into_bst(bst, curr_value)
-in_order_traversal(bst)
+if __name__ == '__main__':
+    # Example usage
+    # Create brand new BST
+    print('Create new BST with passed values')
+    bst = None
+    tree_values = [5, 3, 8, 2, 4, 7, 9]
+    for curr_value in tree_values:
+        bst = insert_into_bst(bst, curr_value)
+    in_order_traversal(bst)
 
-# Add values to an existing BST
-print('\nAdd values to an existing BST')
-bst = insert_into_bst(bst, 1)
-in_order_traversal(bst)
+    # Add values to an existing BST
+    print('\nAdd values to an existing BST')
+    bst = insert_into_bst(bst, 1)
+    in_order_traversal(bst)
