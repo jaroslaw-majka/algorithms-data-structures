9c3c8cf30fd4f7620d2a1a7a3624aa7fc9edb7c0
Jarek <jaroslaw.majka@pm.me>
2024-12-28 15:00:37 +0100

    search reformatted

--- algorithms/search/search_bst.py
+++ algorithms/search/search_bst.py
@@ -2,11 +2,11 @@ from algorithms.trees.helper import create_bst
 
 
 def search_bst(root, value):
-    '''
+    """
         Binary Tree Search is efficient because we can leverage the Tree
         Structure where left node has a lower value than the root value. So
-        with each step we chop off half of the remainer cases.
-    '''
+        with each step we chop off half of the remainder cases.
+    """
     # Base case
     if not root or root.value == value:
         return root
